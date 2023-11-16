f6e73ec63ad98239207ca39e3052110439675115
balaji-githubstore <bala@gmail.com>
2023-11-16 11:35:12 +0530

    Updated Page Objects with non-static

--- src/test/java/com/metlife/pages/MainPage.java
+++ src/test/java/com/metlife/pages/MainPage.java
@@ -1,5 +1,6 @@
 package com.metlife.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class MainPage {
@@ -9,5 +10,12 @@ public class MainPage {
     public MainPage(WebDriver driver)
     {
         this.driver=driver;
+    }
+
+    public void clickOnPatientMenu(){
+        driver.findElement(By.xpath("")).click();
+    }
+    public void clickOnNewSearchMenu(){
+        driver.findElement(By.xpath("")).click();
     }
 }
